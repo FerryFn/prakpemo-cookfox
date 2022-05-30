e76b9a5aecd16ca7aa318449991bf7f8e21311d5
Férry Fathurrahman <73803884+Raixard@users.noreply.github.com>
2022-05-30 20:48:56 +0800

    Clearing POSTTEST6

--- android/app/src/main/kotlin/com/example/posttest6_2009106051_ferry_fathurrahman/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.posttest6_2009106051_ferry_fathurrahman
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- lib/data/palette.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/cupertino.dart';
-
-class Palette {
-  static const Color black = Color(0xFF050517);
-  static const Color white = Color(0xFFFFFFFF);
-  static const Color blue = Color(0xFF5386E4);
-  static const Color orange = Color(0xFFE55812);
-  static const Color green = Color(0xFF12973A);
-}
